4df7cffeed1975a900be6ce2d6a7f7da9a7f5623
krasimirt01 <72243452+krasimirt01@users.noreply.github.com>
2020-10-08 21:22:36 +0300

    Add files via upload

---
+++ src/exercise2/Circle.java
@@ -0,0 +1,20 @@
+package exercise2;
+
+import java.until.Scanner;
+
+public class Circle {
+
+  public static void main (String[] args)  {
+
+    Scanner imput=new Scanner(System.in);
+    System.out.println.("r: ");
+    double r= imput.nextDouble();
+    double C= (2*Math.PI*r);
+    double A= (Math.PI*r*);
+    System.out.println("Circumference:  "+C);
+    System.out.println("Area: "+A);
+
+    
+
+  }
+}
---
+++ src/exercise2/EvenOrOddNmber.java
@@ -0,0 +1,21 @@
+package exercise2;
+
+import java.until.Scanner;
+
+public class EvenOrOddNmber {
+  public static void main (String[] args)
+  Scanner=new Scanner(System.in);
+  System.out.println("Type a number");
+  int a = input.nextInt();
+  if (a%2==0){
+   System.out.println("Number is even");
+ } else {
+   System.out.println("Number is odd");
+  }
+ }
+}
+
+
+
+
+
