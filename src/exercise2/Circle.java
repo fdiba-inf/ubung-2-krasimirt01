19573de58723d0fcba81dfaf33f42931cb80b266
krasimirt01 <72243452+krasimirt01@users.noreply.github.com>
2020-10-09 16:12:48 +0300

    Add files via upload

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -1,18 +1,19 @@
 package exercise2;
 
-import java.until.Scanner;
+import java.util.Scanner;
 
 public class Circle {
 
-  public static void main (String[] args)  {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    Scanner imput=new Scanner(System.in);
-    System.out.println.("r: ");
-    double r= imput.nextDouble();
-    double C= (2*Math.PI*r);
-    double A= (Math.PI*r*);
-    System.out.println("Circumference:  "+C);
-    System.out.println("Area: "+A);
+       System.out.println("r: ");
+       double r= imput.nextDouble();
+       double C= (2*Math.PI*r);
+       double A= (Math.PI*r);
+       System.out.println("Circumference:  "+C);
+       System.out.println("Area: "+A);
+       
 
     
 
